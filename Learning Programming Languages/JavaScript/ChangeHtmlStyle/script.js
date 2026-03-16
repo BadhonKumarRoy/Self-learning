be59d477b1a28d71fd646f5b062c6284b3b371e2
BadhonKumarRoy <badhonroy957@gmail.com>
2026-03-16 14:13:22 +0600

    Day-2

---
+++ Learning Programming Languages/JavaScript/ChangeHtmlStyle/script.js
@@ -0,0 +1,14 @@
+function changeStyle() {
+    const paragraph = document.getElementById("paragraph");
+    paragraph.style.fontSize = "20px";
+    paragraph.style.fontFamily = "Arial, sans-serif";
+    paragraph.style.color = "red";
+}
+function hide() {
+    const p = document.getElementById("p");
+    p.style.display = "none";
+}
+function appear() {
+    const p = document.getElementById("p");
+    p.style.display = "block";
+}
